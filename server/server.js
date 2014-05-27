3377d1c22f5d28a949e039608d7577e2e627d66b
moredhel <hamhut1066@gmail.com>
2014-05-27 17:56:51 +0100

    working sample server

--- server/server.js
+++ server/server.js
@@ -1,58 +1,17 @@
-var sys = require("sys");
-var net = require("net");
-var http = require("http");
-var magic_string = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-var key = "4aRdFZG5uYrEUw8dsNLW6g==";
-
-function createTestServer(){
-    return new testServer();
-};
-
-function testServer(){
-    var server = this;
-    http.Server.call(server, function(){});
-
-    server.addListener("connection", function(){
-            // requests_recv++;
-            });
-
-    server.addListener("request", function(req, res){
-            res.writeHead(200, {"Content-Type": "text/plain"});
-            res.write("okay");
-            res.end();
-            });
-
-    server.addListener("upgrade", function(req, socket, upgradeHead){
-            // done like this as this object cannot be reused https://github.com/joyent/node/issues/1415
-            var sec_accept = require('crypto').createHash('sha1').update(req.headers['sec-websocket-key'] + magic_string).digest('base64');
-            socket.write( "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
-                + "Upgrade: WebSocket\r\n"
-                + "Connection: Upgrade\r\n"
-                + "WebSocket-Origin: http://localhost:8889\r\n"
-                + "Sec-WebSocket-Accept: " + sec_accept + "\r\n"
-                + "WebSocket-Location: ws://localhost:8889/\r\n"
-                + "\r\n"
-                );
-
-            request_upgradeHead = upgradeHead;
-
-            socket.ondata = function(d, start, end){
-            //var data = d.toString('utf8', start, end);
-            var original_data = d.toString('utf8', start, end); 
-            var data = original_data.split('\ufffd')[0].slice(1); 
-            if(data == "kill"){
-            socket.end();
-            } else {
-            sys.puts(data);
-            socket.write("\u0000", "binary");
-            socket.write(data, "utf8");
-            socket.write("\uffff", "binary");
-            }
-            };
+var WebSocketServer = require('ws').Server
+    , wss = new WebSocketServer({port: 8889});
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        //console.log('received: %s', message);
+        try {
+            var input = JSON.parse(message);
+            console.log(input);
+            console.log(JSON.stringify(input));
+            ws.send(JSON.stringify(input));
+        } catch (e) { console.log(e);}
     });
-};
+    ws.addEventListener('close', function() { 
+        console.log('closing'); 
+    });
+});
 
-sys.inherits(testServer, http.Server);
-
-var server = createTestServer();
-server.listen(8889);
