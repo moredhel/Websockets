6fb85a5faa277a1636f2a169bf8634eccbcc332e
moredhel <hamhut1066@gmail.com>
2014-05-27 17:57:08 +0100

    basic client setup

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 //the main javascript file
 
 var conn = new WebSocket('ws://127.0.0.1:8889');
+var x;
 
 conn.onopen = function() { console.log("connection opened");};
 conn.onerror = function() { console.log("error");};
-conn.onmessage = function(msg) { console.log(msg);};
+conn.onmessage = function(msg) { x = JSON.parse(msg.data); }; //add checking TODO
