aec1333d416b4c69079356202dcbc82ed4642469
moredhel <hamhut1066@gmail.com>
2014-05-27 12:38:51 +0100

    using ws module for implementation of websockets

--- server/server.js
+++ server/server.js
@@ -1,8 +1,1 @@
-var net = require('net');
-var port = 8889;
-
-var server = net.createServer(function (c) {
-    c.write('hello\r\n');
-    c.pipe(c);
-    });
-    server.listen(port, 'localhost');
+var ws = require('ws');
