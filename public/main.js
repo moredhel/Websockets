c207e996645ba14d48f76528dc9b65b83c9b4dd1
moredhel <hamhut1066@gmail.com>
2014-05-29 15:07:15 +0100

    add abstraction object for ease of use

--- public/main.js
+++ public/main.js
@@ -1,8 +1,35 @@
 //the main javascript file
 
-var conn = new WebSocket('ws://127.0.0.1:8890');
-var x;
+//var conn = new WebSocket('ws://127.0.0.1:8890');
+//var x;
+var conn = {
+    ws: new WebSocket('ws://127.0.0.1:8890'),
+    msg: '' }
 
-conn.onopen = function() { console.log("connection opened");};
-conn.onerror = function() { console.log("error");};
-conn.onmessage = function(msg) { x = JSON.parse(msg.data); }; //add checking TODO
+var Msg = { //creating the JSON
+    create: function(type, msg) {
+        if(type != 'connection' &&
+            type != 'message' &&
+            type != 'dirlist') {
+            throw 'invalid message';
+        }
+        var msg = {
+            type: type,
+            message: msg || ''};
+        return JSON.stringify(msg);
+    },
+    parse: function(msg) {
+        try {
+            var parsed = JSON.parse(msg);
+            return parsed;
+        } catch (e) {
+            throw e;
+        }
+    }
+}
+
+conn.ws.onopen = function() { console.log("connection opened");};
+conn.ws.onerror = function() { console.log("error");};
+conn.ws.onmessage = function(msg) {
+    conn.msg = Msg.parse(msg.data);
+};
